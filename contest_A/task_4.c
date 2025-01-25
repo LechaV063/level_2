b4311d8fe57017c1d729f2e43e73bac4713a33ee
agvolodin <agvolodin@stdu.info>
2025-01-26 01:32:27 +0400

    add task A4

--- contest_A/task_4.c
+++ contest_A/task_4.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
 
-// На вход программе подается беззнаковое 32-битное целое число N и натуральное число K (1 ≤ K ≤ 31). 
-// Требуется взять K подряд идущих битов числа N так, чтобы полученное число было максимальным. 
+// На вход программе подается беззнаковое 32-битное целое число N и натуральное число K (1 ≤ K ≤ 31).
+// Требуется взять K подряд идущих битов числа N так, чтобы полученное число было максимальным.
 // Программа должна вывести полученное число.
 
+#define MASK_START 0xFFFFFFFF
 
-int main(int argc, char ** argv)
+uint32_t maxNumber(uint32_t number, uint32_t k)
 {
+    int max = 0;
+    uint32_t curMask = ~(MASK_START << k);
+    for (size_t i = 0; i <= 32 - k; i++)
+    {
+        max = ((number & curMask) > max) ? (number & curMask) : max;
+        number >>=1;
+    }
+    return max;
+};
+
+int main(int argc, char **argv)
+{
+    uint32_t N, K;
+    scanf("%u %u", &N, &K);
+    if (K < 1 || K > 31)
+    {
+        exit(EXIT_FAILURE);
+    }
+    // printf("N = %d, K = %d\n", N, K);
+    uint32_t out = maxNumber(N, K);
+    printf("%u\n", out);
 
     return 0;
 }
