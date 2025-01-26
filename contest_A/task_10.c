e2e4d85bbe2484cf73f4df0236356acf4d141659
agvolodin <agvolodin@stdu.info>
2025-01-26 21:26:54 +0400

    A10

--- contest_A/task_10.c
+++ contest_A/task_10.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
-// Необходимо реализовать программу, которой на входе передаётся вещественное число в типе float, 
-// она возвращает порядок который хранится в поле EXP в виде десятичного целого числа. 
+// Необходимо реализовать программу, которой на входе передаётся вещественное число в типе float,
+// она возвращает порядок который хранится в поле EXP в виде десятичного целого числа.
 // Функция должна строго соответствовать прототипу
 // int extractExp(float)
-
-int main(int argc, char ** argv)
+int extractExp(float);
+int main(int argc, char **argv)
 {
-
+    int exp;
+    float ft;
+    scanf("%f", &ft);
+    printf("%d\n", extractExp(ft));
     return 0;
 }
+int extractExp(float numb)
+{
+    int exp = 0;
+    // if (numb < 0)
+    //     numb = -numb;
+    int *pInt = NULL;
+    pInt = (int *)&numb;
+    exp = (*pInt >> 23) & 0xff;
+    return exp;
+};
