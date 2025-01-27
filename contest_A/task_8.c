fababee6d2f3d27827203327c45620139ba228da
agvolodin <agvolodin@stdu.info>
2025-01-27 14:53:24 +0400

    task_9.c

--- contest_A/task_8.c
+++ contest_A/task_8.c
@@ -1,16 +1,75 @@
 #include <stdio.h>
+#include <stdint.h>
+#define BASE 3
+#define MAX_POWER 19
 
 // В стартер боксе Geek Brains есть электронные чашечные весы (вместимость каждой чашки 1 тонна)
-// и гири массами 1г, 3г, 9г, 27г, и т.д. по одной штуке каждой массы. 
-// Менеджер из рекламного отдела взял такие весы и не знает, можно ли взвесить что-нибудь тяжелое и если можно, 
-// сколько гирь придется положить на весы (на любую чашку). Надо помочь ему. 
+// и гири массами 1г, 3г, 9г, 27г, и т.д. по одной штуке каждой массы.
+// Менеджер из рекламного отдела взял такие весы и не знает, можно ли взвесить что-нибудь тяжелое и если можно,
+// сколько гирь придется положить на весы (на любую чашку). Надо помочь ему.
 // Формат входных данных: единственное натуральное число – масса взвешиваемого товара в граммах (не больше 1000000000).
-// Формат выходных данных: единственное целое число – общее количество гирь 
+// Формат выходных данных: единственное целое число – общее количество гирь
 // или число –1 если данный товар взвесить невозможно.
 
+typedef struct item
+{
+    int used;
+    int32_t weigth;
+    uint32_t summBefore;
+} item;
 
-int main(int argc, char ** argv)
+int power(int base, int count)
 {
 
+    int result = 1;
+    for (size_t i = 0; i < count; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    uint32_t weight;
+    int count = 0;
+    scanf("%u", &weight);
+    item set[MAX_POWER];
+    uint32_t leftSumm = 0, rightSumm = 0;
+    uint32_t exp = 0, summ = 0;
+    for (size_t i = 0; i < MAX_POWER; i++)
+    {
+        set[i].summBefore = summ;
+        set[i].used = 0;
+        set[i].weigth = power(BASE, i);
+        summ += set[i].weigth;
+    }
+    for (int i = MAX_POWER - 1; i >= 0; i--)
+    {
+        if (weight >= set[i].weigth)
+        {
+            count++;
+            set[i].used = 1;
+            weight -= set[i].weigth;
+        }
+    }
+    if (weight == 0)
+    {
+        printf("%d\n", count);
+        return 0;
+    }
+    else
+    {
+        for (size_t i = MAX_POWER - 1; i >= 0; i--)
+        {
+            if ((set[i].used == 0) && weight >= set[i].weigth)
+            {
+                count++;
+                set[i].used = 1;
+                weight -= set[i].weigth;
+            }
+        }
+        weight ? printf("%d\n", -1) : printf("%d\n", count);
+    }
     return 0;
 }
--- contest_A/task_9.c
+++ contest_A/task_9.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdint.h>
+#define ARRAY_LENGHT 32
+
 // В программе реализована структура данных:
 // struct pack_array {
 // uint32_t array; // поле для хранения упакованного массива из 0 и 1
@@ -8,10 +11,46 @@
 // Необходимо реализовать программу, которая упаковывает переданный ей массив из 32-ух элементов 0 и 1 в указанную структуру данных.
 // Функция должна строго соответствовать прототипу:
 
-// void array2struct(int [], struct pack_array *)
-
-int main(int argc, char ** argv)
+struct pack_array
 {
+    uint32_t array;      // поле для хранения упакованного массива из 0 и 1
+    uint32_t count0 : 8; // счетчик нулей в array
+    uint32_t count1 : 8; // счетчик единиц в array
+};
+void array2struct(int arr[], struct pack_array *myStructure)
+{
+    myStructure->array = 0;
+    for (size_t i = 0; i < ARRAY_LENGHT; i++)
+    {
+        myStructure->array <<= 1;
+        myStructure->array += arr[i];
+        if (arr[i])
+        {
+            (myStructure->count1)++;
+        }
+        else
+        {
+            (myStructure->count0)++;
+        }
+    }
+};
 
+void structPrint(struct pack_array *myStructure)
+{
+    printf("%u %d %d\n", myStructure->array, myStructure->count0, myStructure->count1);
+}
+int main(int argc, char **argv)
+{
+    int inputArray[ARRAY_LENGHT];
+    struct pack_array myStruct;
+    for (size_t i = 0; i < ARRAY_LENGHT; i++)
+    {
+        scanf("%d", &inputArray[i]);
+    }
+    myStruct.array = 0;
+    myStruct.count0 = 0;
+    myStruct.count1 = 0;
+    array2struct(inputArray, &myStruct);
+    structPrint(&myStruct);
     return 0;
 }
