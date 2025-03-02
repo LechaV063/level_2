b921935f22e464d7a7bb45961008fde0a3443dfc
agvolodin <agvolodin@stdu.info>
2025-03-02 08:17:01 +0400

    initial commit for contest D

---
+++ contest_D/taskD1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+Вид сверху
+В программе описана структура для хранения двоичного дерева:
+typedef struct tree {
+datatype key;
+struct tree *left, *right;
+} tree;
+Необходимо реализовать функцию, которая печатает вид дерева сверху. Функция должна строго соответствовать прототипу:
+void btUpView(tree *root)
+*/
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
---
+++ contest_D/taskD2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+В программе описано двоичное дерево:
+typedef struct tree {
+datatype key;
+struct tree *left, *right;
+struct tree *parent; //ссылка на родителя
+} tree;
+ 
+Требуется реализовать функцию, которая по ключу возвращает адрес соседнего элемента - брата. Если такого ключа нет или у узла нет брата, то необходимо вернуть 0.
+
+Прототип функции:
+tree * findBrother(tree *root, int key)
+*/
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
